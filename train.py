082ea6d314a8243ed87e80462c227c71a19b7cf4
wooseungw <woow070840@gmail.com>
2024-04-09 13:06:29 +0900

    12

--- train.py
+++ train.py
@@ -18,13 +18,7 @@ def select_device():
 # 이전 wandb 실행의 ID 있는 경우만
 #wandb_run_id = "YOUR_PREVIOUS_WANDB_RUN_ID"
 
-# WandbLogger 설정, 이전 실행 재개
-wandb_logger = WandbLogger(
-    project="CCP",
-    log_model="all",
-#    resume="allow",  # 이전 실행이 있으면 재개, 없으면 새 실행 시작
-#    id=wandb_run_id,  # 이전 실행 ID
-)
+
 
 # 체크포인트 콜백 설정
 checkpoint_callback_min_loss = ModelCheckpoint(
@@ -42,18 +36,26 @@ checkpoint_callback_last = ModelCheckpoint(
 # device = select_device()
 # print(f"Selected device: {device}")
 def train():
+    # WandbLogger 설정, 이전 실행 재개
+    wandb_logger = WandbLogger(
+    project="CCP",
+    log_model="all",
+#    resume="allow",  # 이전 실행이 있으면 재개, 없으면 새 실행 시작
+#    id=wandb_run_id,  # 이전 실행 ID
+    
+    )
     # Trainer 객체 생성
     trainer = L.Trainer(
-    logger=wandb_logger,  # Wandb 로거 사용
-    #resume_from_checkpoint=checkpoint_path, 
-    callbacks=[checkpoint_callback_min_loss, checkpoint_callback_last],  # 콜백 리스트에 추가
-    max_epochs=30,  # 최대 에폭 수
+        logger=wandb_logger,  # Wandb 로거 사용
+        #resume_from_checkpoint=checkpoint_path, 
+        callbacks=[checkpoint_callback_min_loss, checkpoint_callback_last],  # 콜백 리스트에 추가
+        max_epochs=30,  # 최대 에폭 수
     )
     model =DPTSegmentationWithCarbon()
     transform = transforms.Compose([transforms.Resize((256, 256)), transforms.ToTensor()])
     fp = 'Dataset/Training/image/SN10_Forest_IMAGE'
-    train_dataloader = DataLoader(CarbonDataset(transform=transform,mode='Train',folder_path=fp), batch_size=4, shuffle=True,persistent_workers=True,num_workers=8)
-    val_dataloader = DataLoader(CarbonDataset(transform=transform,mode='Valid',folder_path=fp), batch_size=4,persistent_workers=True ,num_workers=8)
+    train_dataloader = DataLoader(CarbonDataset(transform=transform,mode='Train',folder_path=fp), batch_size=16, shuffle=True,persistent_workers=True,num_workers=8)
+    val_dataloader = DataLoader(CarbonDataset(transform=transform,mode='Valid',folder_path=fp), batch_size=32,persistent_workers=True ,num_workers=8)
 
     # 모델 학습
     trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
